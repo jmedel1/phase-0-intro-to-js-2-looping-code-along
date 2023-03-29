abb66d8528be97834c243cac1b077ebfe20cb168
jael <jael.medel30@gmail.com>
2023-03-29 16:31:22 -0500

    looping recycle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function writeCards (names, event) {
+function writeCards(names, event) {
     const messages = [];
-    for (let i = 0; i < names.length; i++) {
+    for(let i = 0; i < names.length; i++) {
         const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
         messages.push(message);
     }
@@ -8,10 +8,10 @@ function writeCards (names, event) {
 }
 console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "suprise"));
 
-function countDown (i = 10) {
+function countDown(i = 10) {
     while (i >= 0) {
-        console.log(i);
-        i = i - 1;
-    }
-    
+     console.log(i);
+   i = i - 1;
+ }
 }
+countDown();
